6c56d68e285c7582ba95fab5a54138a71ac9f57c
Ou Frank <oufrank@Oude-MacBook-Pro.local>
2020-03-25 17:10:30 +0800

    final

---
+++ products.py
@@ -0,0 +1,12 @@
+products = []
+while True:
+	name = input('請輸入商品名稱:')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格:')
+	products.append([name,price])
+print(products)
+
+
+for p in products:
+	print(p[0],'價格是',p[1])
